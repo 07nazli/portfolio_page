9502da950b61bc7b3192440e33200f93c81f8f6a
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-06 17:56:02 +0300

    RightSide

--- src/compenents/RightSide/RightSide.jsx
+++ src/compenents/RightSide/RightSide.jsx
@@ -1,8 +1,40 @@
+import React, { useEffect } from 'react';
+import './RightSide.scss';
+
 export default function RightSide() {
+    useEffect(() => {
+        const text = document.querySelector(".sec-text");
+
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Developer";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Freelancer";
+            }, 4000);
+            setTimeout(() => {
+                text.textContent = "Designer";
+            }, 8000); 
+        }
+
+        textLoad();
+        setInterval(textLoad, 12000);
+
+        // Clean-up function for useEffect (optional)
+        return () => clearInterval(textLoad);
+    }, []); // empty dependency array means it runs only once after initial render
+
     return (
-    <div>
-        RIGHTSIDE
-    </div>
+        <header className='right-side'>
+            <div>
+                <h1 className='RightSide__header'>BAŞLIK</h1>
+            </div>
+            <div className='RightSide__container'>
+                <div className="container">
+                    <span className="text first-text">I'm a</span>
+                    <span className="text sec-text"></span>
+                </div>
+            </div>
+        </header>
     );
-  }
-  
+}

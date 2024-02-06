afbe987bb2255e9346504f481d613a004b2b9fba
Esma Nazlı İşbilir <nazliisbilir.business@gmail.com>
2024-02-06 13:56:39 +0300

    portfolio compenent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import SideBar from "./compenents/LeftSide/SideBar";
+import RightSide from "./compenents/RightSide/RightSide";
+import "./Styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__SideBar">
+        <SideBar />
+      </div>
+      <div className="App__RightSide">
+        <RightSide />
+      </div>
     </div>
   );
 }
---
+++ src/compenents/LeftSide/SideBar.jsx
@@ -0,0 +1,42 @@
+import { FaHome } from 'react-icons/fa';
+import { BsFillPersonFill } from "react-icons/bs";
+import { TiThList } from "react-icons/ti";
+import { IoBag } from "react-icons/io5";
+import { TbMessageCircle2Filled } from "react-icons/tb";
+
+export default function SideBar() {
+    return ( 
+    <header className='side-bar'>
+
+        <div className='side-bar__logo'>
+            <a href="https://www.linkedin.com/in/esmanazli007/">Esma Nazlı</a>
+        </div>
+
+        <nav className='side-bar__menu'>
+            <ul className='side-bar__menu__list'>
+                <li className='side-bar__menu__list__item'>
+                    <FaHome />
+                    <a href="#">Home</a>
+                </li>
+                <li className='side-bar__menu__list__item'>
+                    <BsFillPersonFill />
+                    <a href="#">About</a>
+                </li>
+                <li className='side-bar__menu__list__item'>
+                    <TiThList />
+                    <a href="#">Service</a>
+                </li>
+                <li className='side-bar__menu__list__item'>
+                    <IoBag />
+                    <a href="#">Portfolio</a>
+                </li>
+                <li className='side-bar__menu__list__item'>
+                    <TbMessageCircle2Filled />
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+    );
+  }
